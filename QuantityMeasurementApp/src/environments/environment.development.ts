8c9e37195e685de529a26d600ee68a004000e3e8
Arif-Iquba'l <arif04102001@gmail.com>
2026-04-07 15:59:20 +0530

    [Arif Iqubal] : Added environment configuration files

---
+++ QuantityMeasurementApp/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  gatewayUrl: 'http://localhost:8080', // Your real server
+  googleClientId: '511021883613-sgm4qh82a51ig8ctggp005j13nl33bgb.apps.googleusercontent.com',
+  googleRedirectUri: 'http://localhost:4200/auth'
+};
